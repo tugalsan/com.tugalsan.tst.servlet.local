c872ef52daeba2177b3383b667757abc5aeb5f9d
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-02-25 17:39:54 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/tst/servlet/local/Main.java
@@ -0,0 +1,10 @@
+package com.tugalsan.tst.servlet.local;
+
+import com.tugalsan.api.servlet.local.server.TS_SLocal;
+
+class Main {
+
+    public static void main(String... s) {
+        TS_SLocal.main(s);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module com.tugalsan.servlet.local {
+    requires com.tugalsan.api.servlet.local;
+}
